134c3e9788e8d6b518855f1e40855ff822b342b8
andreycmartins <andreybr45@gmail.com>
2024-01-30 13:41:23 -0300

    feat: added initial booking component

--- app/(home)/_components/search.tsx
+++ app/(home)/_components/search.tsx
@@ -8,7 +8,7 @@ const Search = () => {
     return ( 
         <div className="flex items-center gap-2">
             <Input placeholder="busque por uma barbearia" />
-            <Button variant="default" size="icon">
+            <Button variant="default">
                 <SearchIcon size={18}/>
             </Button>
         </div>
