6bc204ced9de544ff2fe56651b7a9e7e2abd5256
andreycmartins <andreybr45@gmail.com>
2024-01-30 16:47:37 -0300

    feat: added footer component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -41,6 +41,16 @@ export default function Home() {
           <BarbershopItem key={barbershop.id} barbershop={barbershop} />
         ))}
       </div>
+
+      <div className="mt-6 mb-[4.5rem]">
+        <h2 className=" px-5 text-sm mb-3 uppercase text-gray-400 font-bol">Ppopulares</h2>
+      </div>
+
+      <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+        {barbershops.map((barbershop) => (
+          <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+        ))}
+      </div>
     </div> 
 
   );
---
+++ app/_components/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <div className="w-full bg-secondary py-4 px-5">
+            <p className="text-gray-400 font-bold">Andrey</p>
+        </div>
+     );
+}
+ 
+export default Footer;
