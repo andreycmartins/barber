c52d32549fe7e8afa739bc7ec47a9c185cc3c135
Andrey Martins <andrey.costs@versatecnologia.com.br>
2024-01-29 23:48:04 -0300

    feat: added welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,17 @@
 import Image from "next/image";
 import Header from "../_components/header";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
   return (
     <div>
       <Header />
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Andrey</h2>
+        <p className="capitalize text-sm">{format(new Date(), "d 'de' MMMM 'de' yyyy", { locale: ptBR })}</p>
+      </div>
     </div> 
   );
 }
