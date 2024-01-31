164a79b911f2346974585c5d2575272d3b8188b8
Andrey Martins <andrey.costs@versatecnologia.com.br>
2024-01-31 19:02:05 -0300

    nem sei

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -6,14 +6,8 @@ import BookingItem from "../_components/booking-item";
 import { db } from "../_lib/prisma";
 import BarbershopItem from "./_components/barbershop-item";
 
-export default function Home() {
-  // const barbershops = await db.barbershop.findMany({})
-  const barbershops = [
-    {id: '1', name: 'barberoasdsadsadasdsadad', address: 'asd', imageUrl: 'https://www.alternativagameselan.com.br/web/fotos/produtos_55_produto-teste-nao-usar_www.alternativagameselan.com.br_zz4ef5edb9b8.png'},
-    {id: '2', name: 'barbeariaetc', address: 'asd', imageUrl: 'https://aquitemplacas.com.br/img/produtos/g/36-atencao-area-de-teste.jpg'},
-    {id: '3', name: 'barbeirosojkepoas', address: 'asd', imageUrl: 'https://www.alternativagameselan.com.br/web/fotos/produtos_55_produto-teste-nao-usar_www.alternativagameselan.com.br_zz4ef5edb9b8.png'},
-    {id: '4', name: 'testearea', address: 'asd', imageUrl: 'https://aquitemplacas.com.br/img/produtos/g/36-atencao-area-de-teste.jpg'}
-  ]
+export default async function Home() {
+  const barbershops = await db.barbershop.findMany({})
   return (
     <div>
       <Header />
@@ -43,7 +37,7 @@ export default function Home() {
       </div>
 
       <div className="mt-6">
-        <h2 className=" px-5 text-sm mb-3 uppercase text-gray-400 font-bol">Ppopulares</h2>
+        <h2 className=" px-5 text-sm mb-3 uppercase text-gray-400 font-bol">Populares</h2>
       </div>
 
       <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
